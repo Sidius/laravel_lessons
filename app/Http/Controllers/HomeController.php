76f5065e132a25d938dd9f44137533269637d8bc
Romanets Pavel <pavelroma24@gmail.com>
2022-06-01 09:43:06 +0300

    add: l_1_9 (Config)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,13 @@ class HomeController extends Controller
 {
     public function index()
     {
+        // l_1_9
+        dump($_ENV['DB_HOST']);
+        dump(env('DB_HOST', 'localhost'));
+        dump($_ENV);
+        dump(config());
+        dump(config('app.timezone'));
+        dump(config('database.connections.mysql.database'));
         return view('home', ['res' => 5, 'name' => 'John']);
     }
 
