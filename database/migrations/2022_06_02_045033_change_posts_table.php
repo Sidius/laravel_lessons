6c89ab981e8b89c1b8a9933d708ab2e9ada209e8
Romanets Pavel <pavelroma24@gmail.com>
2022-06-02 08:21:07 +0300

    add: l_1_10 (DB Migration)

---
+++ database/migrations/2022_06_02_045033_change_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// composer require doctrine/dbal
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title', 100)->change();
+            $table->text('content')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title')->change();
+            $table->text('content')->nullable(false)->change();
+        });
+    }
+};
