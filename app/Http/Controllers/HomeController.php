8d2f2e440491f2b8bd80757612b8dafd1a613193
Romanets Pavel <pavelroma24@gmail.com>
2022-06-03 08:58:19 +0300

    add: l_1_11 (Select, Insert, Update, Delete, Transactions)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
     public function index()
     {
+        // l_1_11
+//        $query = DB::insert("INSERT INTO `posts` (title, content) VALUES (?, ?)", ['Статья 4', 'Описание статьи 4']);
+//        var_dump($query);
+
+//        DB::update("UPDATE `posts` SET created_at = ?, updated_at = ? WHERE created_at IS NULL OR updated_at IS NULL", [NOW(), NOW()]);
+
+//        DB::delete("DELETE FROM `posts` WHERE id = ?", [4]);
+
+        DB::beginTransaction();
+        try {
+            DB::update("UPDATE `posts` SET created_at = ? WHERE created_at IS NULL", [NOW()]);
+//            DB::update("UPDATE `posts` SET updated_at = ? WHERE updated_at2 IS NULL", [NOW()]);
+            DB::update("UPDATE `posts` SET updated_at = ? WHERE updated_at IS NULL", [NOW()]);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            echo $e->getMessage();
+        }
+
+
+        $posts = DB::select("SELECT * FROM `posts`");
+        $posts = DB::select("SELECT * FROM `posts` WHERE id > :id", ['id' => 2]);
+//        var_dump($posts);
+        return $posts;
+
         // l_1_9
         dump($_ENV['DB_HOST']);
         dump(env('DB_HOST', 'localhost'));
