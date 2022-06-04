4270334c05123ce4b3fe5048aaf20c07d4b5ad35
Romanets Pavel <pavelroma24@gmail.com>
2022-06-04 09:13:46 +0300

    add: l_1_12 (Query Builder)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class HomeController extends Controller
 {
@@ -16,30 +17,64 @@ class HomeController extends Controller
 
 //        DB::delete("DELETE FROM `posts` WHERE id = ?", [4]);
 
-        DB::beginTransaction();
-        try {
-            DB::update("UPDATE `posts` SET created_at = ? WHERE created_at IS NULL", [NOW()]);
-//            DB::update("UPDATE `posts` SET updated_at = ? WHERE updated_at2 IS NULL", [NOW()]);
-            DB::update("UPDATE `posts` SET updated_at = ? WHERE updated_at IS NULL", [NOW()]);
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
-            echo $e->getMessage();
-        }
-
-
-        $posts = DB::select("SELECT * FROM `posts`");
-        $posts = DB::select("SELECT * FROM `posts` WHERE id > :id", ['id' => 2]);
-//        var_dump($posts);
-        return $posts;
+//        DB::beginTransaction();
+//        try {
+//            DB::update("UPDATE `posts` SET created_at = ? WHERE created_at IS NULL", [NOW()]);
+////            DB::update("UPDATE `posts` SET updated_at = ? WHERE updated_at2 IS NULL", [NOW()]);
+//            DB::update("UPDATE `posts` SET updated_at = ? WHERE updated_at IS NULL", [NOW()]);
+//            DB::commit();
+//        } catch (\Exception $e) {
+//            DB::rollBack();
+//            echo $e->getMessage();
+//        }
+//
+//
+//        $posts = DB::select("SELECT * FROM `posts`");
+//        $posts = DB::select("SELECT * FROM `posts` WHERE id > :id", ['id' => 2]);
+////        var_dump($posts);
+//        return $posts;
 
         // l_1_9
-        dump($_ENV['DB_HOST']);
-        dump(env('DB_HOST', 'localhost'));
-        dump($_ENV);
-        dump(config());
-        dump(config('app.timezone'));
-        dump(config('database.connections.mysql.database'));
+//        dump($_ENV['DB_HOST']);
+//        dump(env('DB_HOST', 'localhost'));
+//        dump($_ENV);
+//        dump(config());
+//        dump(config('app.timezone'));
+//        dump(config('database.connections.mysql.database'));
+
+        // l_1_12
+//        $data = DB::table('country')->get();
+//        $data = DB::table('country')->limit(5)->get();
+//        $data = DB::table('country')->select('Code', 'Name')->limit(5)->get();
+//        $data = DB::table('country')->select(['Code', 'Name'])->limit(5)->get();
+//        $data = DB::table('country')->select('Code', 'Name')->first();
+//        $data = DB::table('country')->select('Code', 'Name')->orderBy('Code', 'desc')->first();
+
+//        $data = DB::table('city')->select('ID', 'Name')->find(2);
+//        $data = DB::table('city')->select('ID', 'Name')->where('id', 2)->get();
+//        $data = DB::table('city')->select('ID', 'Name')->where('id', '=', 2)->get();
+//        $data = DB::table('city')->select('ID', 'Name')->where('id', '<', 5)->get();
+//        $data = DB::table('city')->select('ID', 'Name')->where([
+//            ['id', '>', 1],
+//            ['id', '<', 5],
+//        ])->get();
+//        $data = DB::table('city')->where('id', '<', 5)->value('Name');
+//        $data = DB::table('country')->limit(10)->pluck('Name');
+
+//        $data = DB::table('country')->count();
+//        $data = DB::table('country')->max('Population');
+//        $data = DB::table('country')->min('Population');
+//        $data = DB::table('country')->sum('Population');
+//        $data = DB::table('country')->avg('Population');
+
+        $data = DB::table('city')->select('CountryCode')->distinct()->get();
+
+        $data = DB::table('city')->select('city.ID', 'city.Name AS city_name', 'country.Code', 'country.Name AS country_name')->limit(10)
+            ->join('country', 'city.CountryCode', '=', 'country.Code')
+            ->orderBy('city.ID')
+            ->get();
+        dd($data);
+
         return view('home', ['res' => 5, 'name' => 'John']);
     }
 
